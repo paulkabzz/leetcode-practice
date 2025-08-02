980aba2dfc200ea31e6526445099821f1201e4c0
Paul Kabulu <pkabulu05@gmail.com>
2025-08-02 18:16:17 +0200

    Time: 15 ms (17.51%) | Memory: 60.8 MB (5.74%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -8,10 +8,7 @@ class Solution {
         if (nums[i] >= 1) s.add(nums[i]);
        }
 
-       System.out.println(s);
-
        for (int i = 1; i <=  s.size() + 1; i++) {
-            // System.out.println(i);
             if (!s.contains(i)) return i;
        }
 
