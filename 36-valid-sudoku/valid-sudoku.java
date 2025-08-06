9c2dc8080234798f3259297c7942935fcaf1b1f7
Paul Kabulu <pkabulu05@gmail.com>
2025-08-06 12:55:00 +0200

    Time: 11 ms (26.89%) | Memory: 44.5 MB (67.22%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,60 @@
+class Solution {
+
+    private boolean checkRows(char[][] A) {
+        Set<Character> s = new HashSet<>();
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (A[i][j] == '.') continue;
+                if (s.contains(A[i][j])) {
+                    System.out.println(A[i][j] + " is repeating in row " + i);
+                    return false;
+                }
+                else s.add(A[i][j]);
+            }
+            s.clear();
+        }
+        return true;
+    }
+
+
+    private  boolean checkCols(char[][] A) {
+        Set<Character> s = new HashSet<>();
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (A[j][i] == '.') continue;
+
+                if (s.contains(A[j][i])) {
+                    System.out.println(A[j][i] + " is repeating in column " + j);
+                    return false;
+                }
+                else s.add(A[j][i]);
+            }
+            s.clear();
+        }
+        return true;
+    }
+
+    private boolean checkBoxes(char[][] A) {
+        Set<Character>[] indices = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            indices[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                int index = (i/3) * 3 + j/3;
+                if (A[i][j] == '.') continue;
+                if (indices[index].contains(A[i][j])) return false;
+                indices[index].add(A[i][j]);
+            }
+        }
+        return true;
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        return checkRows(board) && checkCols(board) && checkBoxes(board);
+    }
+}
